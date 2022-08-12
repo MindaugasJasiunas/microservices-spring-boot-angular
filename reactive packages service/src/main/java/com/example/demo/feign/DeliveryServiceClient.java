7f705cb07877391559de072758d0f1de06a8c3e1
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-12 10:41:18 +0300

    small comment update

--- reactive packages service/src/main/java/com/example/demo/feign/DeliveryServiceClient.java
+++ reactive packages service/src/main/java/com/example/demo/feign/DeliveryServiceClient.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 //@FeignClient(name = "delivery-service", fallbackFactory = DeliveryServiceClientFallbackFactory.class)
 @ReactiveFeignClient(name = "delivery-service", fallbackFactory = DeliveryServiceClientFallbackFactory.class)
 public interface DeliveryServiceClient{
-    @RequestMapping(value = "new-delivery/{parcelPublicId}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json") // URL from payment microservice controller
+    @RequestMapping(value = "new-delivery/{parcelPublicId}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json") // URL from delivery microservice controller
     Mono<DeliveryResponse> createDelivery(@PathVariable("parcelPublicId") UUID parcelPublicId);
 }
