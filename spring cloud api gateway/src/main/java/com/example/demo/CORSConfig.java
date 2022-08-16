32195931103621bd52ffeafdbf18e33fc31a32ac
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-16 17:29:06 +0300

    CORS configured for JS frontend application to be able to access service & duplicate fields(another CORS from downstream service returning same fields) removed by adding 'DedupeResponseHeader' filter. Closes #27

---
+++ spring cloud api gateway/src/main/java/com/example/demo/CORSConfig.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.http.HttpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CORSConfig {
+
+    /*
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        corsConfiguration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request will fail with 403 Invalid CORS request
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept",
+                "X-Requested-With", "Cache-Control", "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        corsConfiguration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+    */
+
+    @Bean
+    CorsWebFilter reactiveCorsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList(
+                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept",
+                "X-Requested-With", "Cache-Control", "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        corsConfig.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        corsConfig.setExposedHeaders(ImmutableList.of(HttpHeaders.AUTHORIZATION)); // expose Authorization header for JS
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+
+}
