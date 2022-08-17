0b2ef075b6267890f008b44e51b4940833fc9f4d
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-17 09:24:39 +0300

    added authentication service & login and register methods, and tested them in App component trying to send HTTP requests #25

--- parcel-frontend/src/app/app.component.ts
+++ parcel-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './service/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parcel-frontend';
+
+  constructor(private authService: AuthenticationService) {
+    // authService.login({username: 'useris', password: 'password'}).subscribe({
+    //   next: (data) => {
+    //     console.log(data);
+    //     console.log(data.headers);
+    //     console.log(data.headers.get('authorization'));
+    //     console.log(data.headers.get('Authorization'));
+    //   },
+    //   error: (err: Error) => {
+    //     console.log(err);
+    //     console.error(err.message);
+    //   },
+    // });
+
+    // authService
+    //   .register({
+    //     username: 'pamella',
+    //     password: 'password',
+    //     firstName: 'Pam',
+    //     lastName: 'Jacobs',
+    //   })
+    //   .subscribe({
+    //     next: (data) => {
+    //       console.log(data);
+    //       console.log(data.headers);
+    //     },
+    //     error: (err: Error) => {
+    //       console.log(err);
+    //       console.log(err.message);
+    //     },
+    //   });
+  }
 }
--- parcel-frontend/src/app/service/authentication.service.ts
+++ parcel-frontend/src/app/service/authentication.service.ts
@@ -1,8 +1,36 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpResponse,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
+  private loginUrl = environment.apiLoginUrl;
+  private registerUrl = environment.apiRegisterUrl;
 
   constructor(private http: HttpClient) {}
+
+  public login(loginRequest: {
+    username: string;
+    password: string;
+  }): Observable<HttpResponse<any>> {
+    return this.http.post<any>(this.loginUrl, loginRequest, {
+      observe: 'response',
+    });
+  }
+
+  public register(registerRequest: {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+  }): Observable<HttpResponse<any>> {
+    return this.http.post<any>(this.registerUrl, registerRequest, {
+      observe: 'response',
+    });
+  }
+
 }
