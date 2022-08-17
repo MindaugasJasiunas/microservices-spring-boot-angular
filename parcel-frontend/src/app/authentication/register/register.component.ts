8028b6a42716de2bcfd7222726e448a921949ec9
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-17 11:03:17 +0300

    Updated register component - working. #25

--- parcel-frontend/src/app/authentication/register/register.component.ts
+++ parcel-frontend/src/app/authentication/register/register.component.ts
@@ -1,26 +1,75 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
   form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
   });
+  private error: string | null = null;
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 
   submit() {
     if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
+      this.subscriptions.push(
+        this.authenticationService
+          .register({
+            username: this.form.get('username')?.value,
+            password: this.form.get('password')?.value,
+            firstName: this.form.get('firstName')?.value,
+            lastName: this.form.get('lastName')?.value
+          })
+          .subscribe({
+            next: (response: HttpResponse<any>) => {
+              console.log(response);
+              this.router.navigate(['/login']);
+            },
+            error: (err: HttpErrorResponse) => {
+              this.error = err.error.message;
+            },
+          })
+      );
     }
   }
 
-  @Input() error: string | null = 'Error occured';
-
-  @Output() submitEM = new EventEmitter();
+  showError(): string | null {
+    if (this.error) {
+      return this.error;
+    } else if (
+      (!this.form.get('username')?.valid &&
+        this.form.get('username')?.touched) ||
+      (!this.form.get('password')?.valid && this.form.get('password')?.touched)
+    ) {
+      return 'Username or password invalid';
+    }
+    return null;
+  }
 }
