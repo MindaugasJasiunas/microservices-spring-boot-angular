cdd2b7b7d809d59b85c54fbbc7ed56e5fe538578
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-17 10:50:20 +0300

    Updated login to work & updated mavigation #25

--- parcel-frontend/src/app/main-nav/main-nav.component.ts
+++ parcel-frontend/src/app/main-nav/main-nav.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit{
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  // username: string | null = null;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthenticationService) {}
+
+  ngOnInit(){
+  }
+
+  getUsername(): string | null {
+    if(this.authService.isLoggedIn()){
+      return this.authService.getUsernameFromLocalCache();
+    }
+    return null;
+  }
+
+  logout(): void{
+    this.authService.logout();
+  }
 
 }
