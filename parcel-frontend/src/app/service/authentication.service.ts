b6bc46c2dc7d1064d18e535856713d77799605a1
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-17 09:47:32 +0300

    authentication service updated to use localStorage for tokens #25

--- parcel-frontend/src/app/service/authentication.service.ts
+++ parcel-frontend/src/app/service/authentication.service.ts
@@ -1,8 +1,6 @@
-import {
-  HttpClient,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -10,6 +8,11 @@ import { environment } from 'src/environments/environment';
 export class AuthenticationService {
   private loginUrl = environment.apiLoginUrl;
   private registerUrl = environment.apiRegisterUrl;
+  private hostRefreshToken = environment.hostRefreshToken;
+  private accessToken: string | null = null;
+  private refreshToken: string | null = null;
+  private loggedInUsername: string | null = null;
+  private jwtHelper = new JwtHelperService();
 
   constructor(private http: HttpClient) {}
 
@@ -31,6 +34,79 @@ export class AuthenticationService {
     return this.http.post<any>(this.registerUrl, registerRequest, {
       observe: 'response',
     });
+  }
+
+  public logout(): void {
+    this.refreshToken = null;
+    this.accessToken = null;
+    this.loggedInUsername = null;
+
+    // remove items from localStorage - token, user information
+    localStorage.removeItem('user');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('accessToken');
+  }
+
+  public getAccessToken(refreshToken: string): Observable<HttpResponse<any>> {
+    this.isLoggedIn();
+    return this.http.get<string>(this.hostRefreshToken, {
+      observe: 'response',
+      headers: new HttpHeaders({ Authorization: refreshToken }),
+    });
+  }
+
+
+  public saveRefreshToken(refreshToken: string): void {
+    this.refreshToken = refreshToken;
+    localStorage.setItem('refreshToken', refreshToken);
+  }
+
+  public saveAccessToken(accessToken: string): void {
+    this.accessToken = accessToken;
+    localStorage.setItem('accessToken', accessToken);
+  }
+
+  public addUsernameToLocalCache(username: string): void {
+    this.loggedInUsername = username;
+    localStorage.setItem('user', JSON.stringify(username));
+  }
+
+  public getUsernameFromLocalCache(): string {
+    return JSON.parse(localStorage.getItem('user')!);
+  }
+
+  public loadAccessTokenFromLocalCache(): string {
+    this.accessToken = localStorage.getItem('accessToken')!;
+    return localStorage.getItem('accessToken')!;
+  }
+
+  public loadRefreshTokenFromLocalCache(): string {
+    this.accessToken = localStorage.getItem('accessToken')!;
+    return localStorage.getItem('accessToken')!;
+  }
+
+  public isLoggedIn(): boolean {
+    // if refresh token is valid - user is logged in
+    this.loadRefreshTokenFromLocalCache();
+    if (this.refreshToken != null && this.refreshToken !== '') {
+      if (this.jwtHelper.decodeToken(this.refreshToken).sub != null || '') {
+        if (!this.jwtHelper.isTokenExpired(this.refreshToken)) {
+          // this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          this.addUsernameToLocalCache(
+            this.jwtHelper.decodeToken(this.refreshToken).sub
+          );
+          return true;
+        }
+      }
+    }
+    // if there is no token - logout
+    this.logout();
+    return false;
+  }
+
+
+  public isTokenExpired(token: string | undefined){
+    return this.jwtHelper.isTokenExpired(token);
   }
 
 }
