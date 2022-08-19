dfc5789e5c0e3e42665261bee79131ae46d43aa2
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-19 11:12:19 +0300

    'send-package' component updated to use Angular Material. #25

--- parcel-frontend/src/app/material/material.module.ts
+++ parcel-frontend/src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const materialComponents = [
   MatButtonModule,
@@ -19,7 +20,8 @@ const materialComponents = [
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
-  MatListModule
+  MatListModule,
+  MatCheckboxModule
 ];
 
 
