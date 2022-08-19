5e96e60cd1cbf7f9a7d3a1adb489003b12d0df85
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-08-19 11:49:05 +0300

    'package-info' component updated to handle errors - package not found, service unavailable, gateway is down, others. #25

--- parcel-frontend/src/app/packages/package-info/package-info.component.ts
+++ parcel-frontend/src/app/packages/package-info/package-info.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { catchError, Observable, Subscription, throwError } from 'rxjs';
 import { Package } from 'src/app/model/package.model';
 import { ParcelService } from 'src/app/service/parcel.service';
 
@@ -15,13 +15,37 @@ export class PackageInfoComponent implements OnInit, OnDestroy {
   public parcel$: Observable<Package> | undefined;
   // public parcel: Package | undefined;
   // private subscriptions: Subscription[] = [];
+  public nonExistentPackage = false;
+  public PACKAGE_DOES_NOT_EXIST_MSG = 'Package you are searching does not exist';
+  public serverError = false;
+  public SERVER_ERROR_MSG = 'Opps! Something went wrong. Please try again later';
+
 
   constructor(private route: ActivatedRoute, private parcelService: ParcelService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('trackingNumber')!;
 
-    this.parcel$ = this.parcelService.trackParcel(this.id);
+    this.parcel$ = this.parcelService.trackParcel(this.id)
+    .pipe(
+      // catch error if occurs
+      catchError(
+        (error: HttpErrorResponse): Observable<any> => {
+
+            if(error.status === 500 && error.error.message?.includes('Package with provided tracking number not found')){
+                this.nonExistentPackage=true;
+            }else if(error.status === 0 || error.status === 503 || error.status === 500 ){
+              // status=0 - gateway down
+              // status=503 - service unavailable
+              // status=500 - smth else (like service error-out but not unregistered yet)
+                this.serverError = true;
+            }
+            console.log(error);
+
+            return throwError(() => error);
+        },
+      )
+    );
     // this.subscriptions.push(
     //   this.parcelService.trackParcel(this.id).subscribe({
     //       next: (data: Package) => {
